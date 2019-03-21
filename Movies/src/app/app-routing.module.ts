f7e1f52f39e8eeba464fc6835bacd2cb8684525d
Casey Randall <42552122+gospel02@users.noreply.github.com>
2019-03-20 19:21:11 -0600

    Added Routing

--- Movies/src/app/app-routing.module.ts
+++ Movies/src/app/app-routing.module.ts
@@ -2,11 +2,35 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { MoviedetailComponent } from './moviedetail/moviedetail.component';
+import { MoviesOfUserComponent } from './movies-of-user/movies-of-user.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomepageComponent },
-  { path: 'moviedetail/:movieID', component: MoviedetailComponent }
+  { 
+    path: '', 
+    component: HomepageComponent
+  },
+  { 
+    path: 'moviedetail/:movieID', 
+    component: MoviedetailComponent
+  },
+  { 
+    path: 'homepage', 
+    component: HomepageComponent
+  },
+  { 
+    path: 'movies-of-user', 
+    component: MoviesOfUserComponent
+  },
+  { 
+    path: '', 
+    redirectTo: 'homepage', 
+    pathMatch: 'full' 
+  },
+  { path: '**', 
+    redirectTo: 'homepage', 
+    pathMatch: 'full' 
+  }
 ];
 
 @NgModule({
