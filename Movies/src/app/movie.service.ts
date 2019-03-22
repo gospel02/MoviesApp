dfef760b0353b47119b43432a43f9e6c31af027d
Casey Randall <42552122+gospel02@users.noreply.github.com>
2019-03-21 23:09:12 -0600

    Updates to movie service and CSS

--- Movies/src/app/movie.service.ts
+++ Movies/src/app/movie.service.ts
@@ -5,33 +5,33 @@ import { HttpClient } from '@angular/common/http';
 export class MovieService {
 
   private url = 'https://api.themoviedb.org/3/';
-  private api_key = 'c8deafc3aa91fe5f48fab331e2b8467d';
+  private apikey = 'c8deafc3aa91fe5f48fab331e2b8467d';
   private language = 'en-US';
   private region = 'US';
-  private movie_string: string;
+  private moviestring: string;
   private id: number;
 
   constructor(public _http: HttpClient) { }
 
   searchMovie(movie: string) {
-    this.movie_string = movie;
-    return this._http.get(this.url + 'search/movie?query=' + this.movie_string + '&api_key=' + this.api_key);
+    this.moviestring = movie;
+    return this._http.get(this.url + 'search/movie?query=' + this.moviestring + '&api_key=' + this.apikey + '&language=' + this.language + '&region=' + this.region);
   }
 
   getTopRatedMovies() {
-    return this._http.get(this.url + 'discover/movie?sort_by=vote_average.desc' + '&api_key=' + this.api_key);
+    return this._http.get(this.url + 'discover/movie?sort_by=vote_average.desc' + '&api_key=' + this.apikey + '&language=' + this.language + '&region=' + this.region);
   }
 
   getUpcomingMovies() {
-    return this._http.get(this.url + 'discover/movie?primary_release_date.gte=2019-03-30&primary_release_date.lte=2019-05-15' + '&api_key=' + this.api_key + '&language=' + this.language + '&region=' + this.region);
+    return this._http.get(this.url + 'discover/movie?primary_release_date.gte=2019-03-30&primary_release_date.lte=2019-05-15' + '&api_key=' + this.apikey + '&language=' + this.language + '&region=' + this.region);
   }
 
   getPopularMovies() {
-    return this._http.get(this.url + 'discover/movie?sort_by=popularity.desc' + '&api_key=' + this.api_key + '&language=' + this.language + '&region=' + this.region);
+    return this._http.get(this.url + 'discover/movie?sort_by=popularity.desc' + '&api_key=' + this.apikey + '&language=' + this.language + '&region=' + this.region);
   }
 
   getMovie(id: number) {
-    return this._http.get(this.url + 'movie/' + id + '?api_key=' + this.api_key);
+    return this._http.get(this.url + 'movie/' + id + '?api_key=' + this.apikey);
   }
 
 }
